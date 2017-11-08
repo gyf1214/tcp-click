fdbe9cad614eac04d06c0653c298ccd866c85f27
gyf1214 <gyf1214@gmail.com>
2017-11-09 00:18:15 +0800

    simple test

---
+++ src/simple_test.cc
@@ -0,0 +1,19 @@
+// implement simple push element
+#include <click/config.h>
+#include "simple_test.hh"
+CLICK_DECLS
+
+SimpleTestElement::SimpleTestElement() {}
+SimpleTestElement::~SimpleTestElement() {}
+
+int SimpleTestElement::configure(Vector<String> &, ErrorHandler *) {
+    return 0;
+}
+
+void SimpleTestElement::push(int, Packet *p) {
+    click_chatter("got a packet of size %d", p->length());
+    output(0).push(p);
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(SimpleTestElement)

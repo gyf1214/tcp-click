d19c2bb29475541e8d6cf665c31b754500292767
gyf1214 <gyf1214@gmail.com>
2017-11-10 11:34:11 +0800

    lsp_frontend

---
+++ src/infra_log.hh
@@ -0,0 +1,10 @@
+#ifndef __CLICK_INFRA_LOG
+#define __CLICK_INFRA_LOG
+#include <click/config.h>
+CLICK_DECLS
+
+#define Log(fmt, ...) click_chatter("%s@%x\t[Info] " fmt, this->class_name(), this->self, ##__VA_ARGS__)
+#define Warn(fmt, ...) click_chatter("%s@%x\t>[Warn] " fmt, this->class_name(), this->self, ##__VA_ARGS__)
+
+CLICK_ENDDECLS
+#endif
---
+++ src/lsp_packet.hh
@@ -0,0 +1,39 @@
+#ifndef __CLICK_ISP_PACKET
+#define __CLICK_ISP_PACKET
+#include <click/config.h>
+#include <click/integers.hh>
+CLICK_DECLS
+
+enum LspType {
+    LspAck = 0,
+    LspHello = 1,
+    LspSequence = 2,
+};
+
+const uint16_t LspMagic = 0x35fc;
+const uint8_t LspProto = 61;
+
+struct LspSequenceData {
+    uint32_t sequence;
+    uint32_t count;
+    uint32_t entry[0];
+};
+
+struct LspHeader {
+    uint16_t magic;
+    uint16_t type;
+    LspSequenceData data[0];
+
+    void Init(LspType);
+};
+
+const size_t LspSizeShort = sizeof(LspHeader);
+const size_t LspSize = LspSizeShort + sizeof(LspSequenceData);
+
+inline void LspHeader::Init(LspType t) {
+    magic = LspMagic;
+    type = t;
+}
+
+CLICK_ENDDECLS
+#endif

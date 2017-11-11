b9982bc54b9ed7c66cd77ab0150efd5f675e3d1a
gyf1214 <gyf1214@gmail.com>
2017-11-11 11:51:20 +0800

    .

--- src/ip_frontend.cc
+++ src/ip_frontend.cc
@@ -24,6 +24,7 @@ void IpFrontend::push(int, Packet *p) {
     WritablePacket *q = p->uniqueify();
     IpHeader *ip_q = (IpHeader *)q->data();
     uint16_t checksum = click_in_cksum((unsigned char *)ip_q, IpSize);
+    Log("%08x", checksum);
     if (ip_q->magic != IpMagic || checksum != ip_q->checksum) {
         Warn("ip header error");
         // discard on header error

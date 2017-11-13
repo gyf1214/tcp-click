603efd17c300cf5fe6f9bf4d9e2656816267a622
gyf1214 <gyf1214@gmail.com>
2017-11-13 22:21:11 +0800

    .

--- src/infra_log.hh
+++ src/infra_log.hh
@@ -1,10 +1,13 @@
 #ifndef __CLICK_INFRA_LOG
 #define __CLICK_INFRA_LOG
 #include <click/config.h>
+#include <click/timestamp.hh>
 CLICK_DECLS
 
-#define Log(fmt, ...) click_chatter("%s@%08x\t[Info] " fmt, this->class_name(), this->self, ##__VA_ARGS__)
-#define Warn(fmt, ...) click_chatter("%s@%08x\t[Warn] " fmt, this->class_name(), this->self, ##__VA_ARGS__)
+#define Log(fmt, ...) click_chatter("%d\t%s@%08x\t[Info] " fmt, Timestamp::now().msec(),\
+this->class_name(), this->self, ##__VA_ARGS__)
+#define Warn(fmt, ...) click_chatter("%d\t%s@%08x\t[Warn] " fmt, Timestamp::now().msec(),\
+this->class_name(), this->self, ##__VA_ARGS__)
 
 CLICK_ENDDECLS
 #endif
